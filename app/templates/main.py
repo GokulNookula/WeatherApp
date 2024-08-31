8bad304df4d9143cb071c3671b430772a3453c3d
Gokul Nookula <gnook002@ucr.edu>
2024-08-31 01:12:53 -0700

    Creating a tech stack with Python, FastAPI, Requests and using templates

--- app/templates/main.py
+++ app/templates/main.py
@@ -1,1 +1,10 @@
-print("Hello testing")
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+print("Hello")
