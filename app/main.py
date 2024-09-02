25ced68d0c2b3647b7badc9d603335623affb971
Gokul Nookula <gnook002@ucr.edu>
2024-09-02 00:44:04 -0700

    Adding error handling to my code to validate if the user is inputting the valid city name

--- app/main.py
+++ app/main.py
@@ -16,12 +16,21 @@ async def readRoot(request:Request):
     return templates.TemplateResponse("index.html",{"request": request})
 
 @app.post("/", response_class= HTMLResponse)
-async def getWeather(request= Request, city: str = Form(...)):
+async def getWeather(request: Request, city: str = Form(...)):
+    city = city.lower()
     apiKey = secret.api
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric'
 
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
     
-    weather = response.json()
-    return templates.TemplateResponse('index.html', {'request': request, 'weather': weather})
+    if response.status_code == 200:
+        weather = response.json()
+        if city not in weather.get("name", "").lower():
+            errorMessage = "Please enter a valid city name, not a state or country."
+            return templates.TemplateResponse('index.html', {'request': request, 'error': errorMessage})
+        else:
+            return templates.TemplateResponse('index.html', {'request': request, 'weather': weather})
+    else:
+        errorMessage = "Could not retrieve weather data. Please try again."
+        return templates.TemplateResponse('index.html', {'request': request, 'error': errorMessage})
